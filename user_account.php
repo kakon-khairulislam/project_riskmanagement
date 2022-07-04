174f9e0752d2c667bc4d3513d8f527902b3ed57f
kakon-khairulislam <99829285+kakon-khairulislam@users.noreply.github.com>
2022-07-04 17:45:53 +0600

    Add files via upload

---
+++ user_account.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>User_Account</title>
+
+<style type="text/css">
+		.red{
+			color: #d41c0f;
+			font-family: Perpetua;
+		}</style>
+
+</head>
+<body style=" padding-top: 80px;">
+    
+		<table border="1" width="50%">
+			<tr>
+				<td width="50%">
+                <h4>Account For Medi_Team_Member</h4><hr><br>
+					<ul style="list-style-type:disc;">
+						<li><a href="">Dashboard</a></li>
+						<li><a href="view_profile.php">View Profile</a></li>
+						<li><a href="edit_profile.php">Edit Profile</a></li>
+						<li><a href="change_pp.php">Change Profile Picture</a></li>
+						<li><a href="change_pass.php">Change Password</a></li>
+						<li><a href="provideMedicine.php">Provide Medicine</a></li>
+						<li><a href="fund.php">Donate</a></li>
+						<form method="POST" action="logout.php">
+							<li><button type="submit" name="logout_btn">Log out</button></li>
+						</form>
+					</ul>
+				</td>
+				<td width="30%">
+					<h2 align="center">Welcome </h2>
+				</td>
+			</tr>
+		</table>
+
+			</td>
+		</tr>
+	
+</body>
+</html>
